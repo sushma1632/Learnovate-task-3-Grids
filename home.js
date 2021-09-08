9b5b0f950764f31abec884bc86a9cc3fcffdd3ee
sushma1632 <71563676+sushma1632@users.noreply.github.com>
2021-09-08 22:45:38 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,36 @@
+function girl1(){
+    alert("Name: Hello this is Rebecca Stanley.I am an Entrepreneur.");
+}
+
+function girl2(){
+    alert("Name: Hello this is Shakira Tanny.I am a Web Developer.");
+}
+
+
+function girl3(){
+    alert("Name: Hello this is Shanaya Shetty.I am a Full Stack Developer.");
+}
+
+function girl4(){
+    alert("Name: Hello this is Clara Berry.I am a Teacher.");
+}
+
+function girl5(){
+    alert("Name: Hello this is Delena Andrew.I am a Lawyer.");
+}
+
+function girl6(){
+    alert("Name: Hello this is Hannah Clifton.I am a Doctor.");
+}
+
+function girl7(){
+    alert("Name: Hello this is Kristen James.I am a Civil Engineer");
+}
+
+function girl8(){
+    alert("Name: Hello this is Anna Sebastian.I am a Photographer.");
+}
+
+function girl9(){
+    alert("Name: Hello this is Elizabeth Cooper.I am an Artist.");
+}
